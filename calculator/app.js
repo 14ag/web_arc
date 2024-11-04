891a07257518c0728096bd52b7cfdddd73c9faa4
14ag <muriukipn@gmail.com>
2024-11-04 23:40:03 +0300

    4.11

--- calculator/app.js
+++ calculator/app.js
@@ -1,13 +1,23 @@
 //figure out whatthe website does okay... a calculator
+console.log("test this thang");
 
 //get elements by id class tag ... do query selector (all)
+let buttons = Array.from(document.querySelectorAll('button'))
+let screen1 = document.querySelector('[name="input"]');
+let screen2 = document.querySelector('[name="result"]');
+
 
 //create elements
+buttons.forEach(function(x){
+    x.addEventListener('click',function(){
+        screen1.value += x.value
+        })
+    })
 
 // make at least 1 thing spin
 
-class calculator {
-    constructor(operand,sign) {
+// class calculator {
+//     constructor(operand,sign) {
         
-    }
-}
+//     }
+// }
