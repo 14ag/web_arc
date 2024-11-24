a7f94a7982dbefe358d22730bb363ca1e2398d67
14ag <muriukipn@gmail.com>
2024-11-24 07:52:21 +0300

    24.11

--- calculator/app_copy.js
+++ calculator/app_copy.js
@@ -1,21 +1,22 @@
 //figure out whatthe website does okay... a calculator
 
-let input=[]
-
-function inputHandler(x){
-    number=isNaN(x)
-    switch (number) {
-        case true:
-            +=x
-            break;
-        case false:
-            
-            break;
-    
+let op=[]
+let i=0
+function inputHandler(x,y){
+    if (y=="n"){
+        console.log(x)
+        // op[i]+=x
+    } else { 
+    switch (y) {
+        case "ac":
+            console.log(y)
+            break;    
         default:
+            console.log(y)
             break;
     }
     
+    }
 }
 
 //get elements by id class tag ... do query selector (all)
@@ -28,61 +29,6 @@ let screen2 = document.querySelector('.result');
 buttons.forEach(function(x){
     x.addEventListener('click',function(){
         screen1.value += x.value
-        inputHandler(x.value)
+        inputHandler(x.value,x.name)
         })
     })
-
-
-
-
-
-
-
-
-
-//classes, object, methods, queues, stacks?
-
-
-
-
-
-
-
-// --------------pseudocode-----------------//
- // create general array arithmetics and put these inside
-
- // when number is pressed, add it to an string or array "operand[1]"
-
- // when non number is pressed, handle_sign() as follows:
-    // if if number of operands is 0
-        // plus, minus, add sign
-        // times, divide ignore input
-                
-    // if number of operands is 1
-        // plus, minus, times, divide add sign
-        
-    // if number of operands is more than 1:
-        // times, divide adds sign
-        // plus adds plus sign to array then makes operand[++1]
-        // minus adds plus sign to array then makes operand[++1] that is negative
-         
-    // if equals, compute
-        // if last value in array is times or divide:
-            // spit error
-        // if compute success:
-            // set answer as operand[1]
-        // add the arithmetic to history
-            // if its the first entry
-                // create a history button
-         
-    // if del, delete thing in the array
-        // pop from array
-        // delete last char from input screen
-         
-    // if ac, clear all
-        // clear screen
-            // clear input screen
-            // reset results
-        // reset operands
-    
-
