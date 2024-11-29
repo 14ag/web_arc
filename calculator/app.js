3811e51b53bd1f24695ab48b03af6c4d8f5c1a5e
14ag <muriukipn@gmail.com>
2024-11-30 00:54:10 +0300

    30.11

--- calculator/app.js
+++ calculator/app.js
@@ -1,88 +1,5 @@
 //figure out whatthe website does okay... a calculator
 
-let input=[]
-if (isNaN(input[input.length-1])==true){
-    throw new Error("syntax error");
-    
-}
-
-class Calculator {
-    constructor(q){
-        this.statement=q
-        this.running=0
-        // {"-","+","*","/"}={"subtract","add","multiply","divide"}
-        order=["/","*","+","-"]
-
-    }
-    ac(){}
-    del(){}
-    equals(){
-        for (let i = 0; i < order.length; i++) {
-            //loop over array looking for order[i] then take found[+1] and [-1] then pass them to be evaluated in the Basic class
-            Calculator.operand();
-            
-        }
-    }
-    history(){}
-
-
-    }
-    
-
-
-class Basic extends Calculator {
-    constructor(a,b) { 
-        this.a=a
-        this.b=b
-        }
-    add(a,b){
-        return a+b
-    }
-    subtract(a,b){
-        return a-b
-    }
-    divide(a,b){
-        if (b==0){
-            throw new Error("dividing by zero????");
-            
-        }
-        return a/b
-    }
-    multiply(a,b){
-        return a*b
-    }
-}
-
-class output {
-    constructor(parameters) {
-        
-    }
-}
-
-
-
-
-//get elements by id class tag ... do query selector (all)
-let buttons = Array.from(document.querySelectorAll('button'))
-let screen1 = document.querySelector('.input');
-let screen2 = document.querySelector('.result');
-
-
-//create elements, attributes, tags
-buttons.forEach(function(x){
-    x.addEventListener('click',function(){
-        screen1.value += x.value
-        parseinput(x.value)
-        })
-    })
-
-
-//classes, object, methods, queues, stacks?
-
-
-
-
-
 
 
 // --------------pseudocode-----------------//
@@ -122,4 +39,129 @@ buttons.forEach(function(x){
             // reset results
         // reset operands
     
+
+
+
+
+//get elements by id class tag ... do query selector (all)
+let buttons = Array.from(document.querySelectorAll('button'))
+let screen1 = document.querySelector('.input');
+let screen2 = document.querySelector('.result');
+let op=[]
+let i = 0
+
+if (isNaN(input[input.length-1])==true){
+    throw new Error("syntax error");
+    
+}
+
+
+
+class input {
+    inputHandler(x, y, z) {
+    if (y == "n") {
+        op[i] += x;
+    } else {
+        switch (z) {
+            case "operator":
+                op.push(x)
+                i += 2;
+                break;
+            case "operation":
+                Calculator.y()
+                break;
+            case "run":
+                Calculator.y(op)
+                break;
+            default:
+                console.log("errorrrr")
+                break;
+        }
+
+    }
+    return op;
+}
+}
+
+
+
+
+class Calculator {
+    constructor(q) {
+        this.statement = q
+        this.running = 0
+        // {"-","+","*","/"}={"subtract","add","multiply","divide"}
+        order = ["/", "*", "+", "-"]
+        sign = {
+            "/": divide,
+            "*": multiply,
+            "+": add,
+            "-": subtract,
+        }
+
+    }
+    ac() { }
+    del() { }
+    equals() {
+        let entry = op.toSpliced(op.length,0,"=").toString(); //to be used in history
+        for (let i = 0; i < order.length; i++) {
+            //get no. occurences
+            let counter = op.filter((x) => x == order[i]).length
+
+            //loop over array looking for order[i] 
+            for (let ii = 0; ii < counter; ii++) {
+                let sub_op = op.indexOf(order[i])
+                // then take found[+1] and [-1] then pass them to be evaluated in the Basic class
+                let a = Calculator.sign['order[i]']
+                let b = Basic['a'](op[sub_op - 1], order[sub_op + 1])
+                op.splice(sub_op - 1,3)
+                op.splice(sub_op - 1,0,b)
+
+            }
+        }
+        return op.toString
+    }
+    history() {
+        //add entry
+     }
+}
+    
+
+
+class Basic extends Calculator {
+    add(a,b){
+        return a+b
+    }
+    subtract(a,b){
+        return a-b
+    }
+    divide(a,b){
+        if (b==0){
+            throw new Error("dividing by zero????");
+        }
+        return a/b
+    }
+    multiply(a,b){
+        return a*b
+    }
+}
+
+class output {
+    constructor(parameters) {
+        
+    }
+}
+
+
+
+
+//create elements, attributes, tags
+buttons.forEach(function(x){
+    x.addEventListener('click',function(){
+        screen1.value += x.value
+        parseinput(x.value)
+        })
+    })
+
+
 
--- calculator/app_copy.js
+++ calculator/app_copy.js
@@ -1,34 +1,36 @@
 //figure out whatthe website does okay... a calculator
 
-let op=[]
-let i=0
-function inputHandler(x,y,z){
-    if (y=="n"){
-        op[i]+=x
-    } else { 
-    switch (z) {
-        case "operator":
-            op.push(x)
-            i+=2
-            break;    
-        case "operation":
-            Calculator.y()
-            break;    
-        case "run":
-            Calculator.y(op)
-            break;    
-        default:
-            console.log("errorrrr")
-            break;
-    }
-    
-    }
-}
+let op = []
+let i = 0
+class input {
+    inputHandler(x, y, z) {
+    if (y == "n") {
+        op[i] += x;
+    } else {
+        switch (z) {
+            case "operator":
+                op.push(x)
+                i += 2;
+                break;
+            case "operation":
+                Calculator.y()
+                break;
+            case "run":
+                Calculator.y(op)
+                break;
+            default:
+                console.log("errorrrr")
+                break;
+        }
 
+    }
+    return op;
+}
+}  
 //create elements, attributes, tags
-buttons.forEach(function(x){
-    x.addEventListener('click',function(){
+buttons.forEach(function (x) {
+    x.addEventListener('click', function () {
         screen1.value += x.value
-        inputHandler(x.value,x.name,x.class)
-        })
+        inputHandler(x.value, x.name, x.class)
     })
+})
