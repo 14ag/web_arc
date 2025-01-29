f908afc15d906e4f6f3b0b92f8a702ec453044b4
14ag <muriukipn@gmail.com>
2025-01-29 03:48:44 +0300

    first working version lol

--- calculator/app.js
+++ calculator/app.js
@@ -2,33 +2,32 @@
 let buttons = Array.from(document.querySelectorAll('button'))
 let input_screen = document.querySelector('.input');
 let result_screen = document.querySelector('.result');
-let running = 0
 let entry = ""
-let stack = []
+let stack = [""]
 let stack_pointer = 0 //stack pointer
-let sign = {
-    "/": (a, b)=> {
+let sign = { //property/key :value
+    "*": (a, b) => {
+        return a * b;
+    },
+    "+": (a, b) => {
+        return a + b
+    },
+    "-": (a, b) => {
+        return a - b
+    },
+    "/": (a, b) => {
         if (b == 0) {
             throw new Error("dividing by zero????");
         }
         return a / b
     },
-    "*": (a, b)=>{
-        return a * b;
-    },
-    "+": (a, b)=>{
-        return a + b
-    },
-    "-": (a, b)=>{
-        return a - b
-    },
 }
 // order of opps stated here
-let order = sign.sort(function(a,b){
+let order = Object.keys(sign).sort(function (a, b) {
     let O = ["/", "*", "+", "-"] //here 
-    if (O.indexOf(a)<O.indexOf(b)){
+    if (O.indexOf(a) < O.indexOf(b)) {
         return -1
-    } else{
+    } else {
         return 1
     }
 })
@@ -42,46 +41,45 @@ buttons.forEach(function (x) {
     x.addEventListener('click',
         function () {
             // input_screen.value = x.value
-            return input_handler(x.value, x.name, x.className)
+            input_handler(x.value, x.name, x.className)
+            input_screen.value = stack.toString()
+
         })
 })
 
 function input_handler(x = "", y = "", z = "") { //x- value,  y-name,  z-class
+    x = x.toString()
     if (y == "n") {
-        if (isNaN(stack[stack_pointer])) {
-            if (stack_pointer === 0) {
-                stack[stack_pointer] = x
-            } else {
-                stack_pointer++
-                stack[stack_pointer] = x
-            }
-            // input_screen.value= "yes" //debug
-        } else {
-            stack[stack_pointer] = stack[stack_pointer].toString() + x
+        if (order.includes(stack[stack_pointer])) {
+            stack_pointer++
         }
+        if (stack[stack_pointer] == undefined) {
+            stack[stack_pointer] = ""
+        }
+        stack[stack_pointer] += x
     } else {
         switch (z) { //classname
             case "operator":
                 //add the sign
-                stack.push(x)
-                stack_pointer++
-                input_screen.value = stack
+                if (stack[stack_pointer] != 0) {
+                    stack.push(x)
+                    stack_pointer++
+                }
                 break;
             case "operation":
-                Calculator[y](stack) // calls the function stored in y
+                eval(y + "()") // calls the function stored in y
                 break;
         }
     }
-    input_screen.value = stack
 }
 
-class Calculator{
-equals() {
-    let entry = stack.toSpliced(stack.length, 0, "=").toString(); //to be used in history
+function equals() {
+    
+    // entry += stack.toSpliced(stack.length, 0, "=").toString(); //to be used in history
 
     //check if last char is NaN
-    if (isNaN(stack[stack.length - 1]) == true) {
-        throw new Error("syntax error");
+    if (order.includes(stack[stack_pointer])) {
+        return;
     }
 
     for (let i = 0; i < order.length; i++) {
@@ -90,40 +88,30 @@ equals() {
 
         //loop over array looking for order[i] 
         for (let ii = 0; ii < counter; ii++) {
-            let sub_op = stack.indexOf(order[i])
-            // then take found[+1] and [-1] then pass them to be evaluated in the Basic class
-            let a = sign[order[i]]
-            console.log(a)
-
-            stack.splice(sub_op - 1, 3)
-            stack.splice(sub_op - 1, 0, Calculator[a](stack[sub_op - 1], stack[sub_op + 1]))
-            console.log(stack)
-
+            let sub_op = stack.indexOf(order[i]) //a number eg 1
+            // then take found[+1] and [-1] then pass them to be evaluated in the sign object
+            //stack[sub_op] is currentOperatorSymbol= 
+            stack.splice(sub_op - 1, 3, sign[stack[sub_op]](+stack[sub_op - 1], +stack[sub_op + 1]))
         }
     }
 
-    running = stack.toString()
-    console.log("")
-    console.log("")
-    console.log("----------------------")
-    return stack.toString()
+    stack_pointer = 1
+    result_screen.value = +stack.toString()
 
 }
 
-del() {
-    stack.splice(-1)
-    screen1.value = stack.pop()
-    Calculator.del()
-}
+function del() {
+    if (stack.length != 0) {
+        if (stack[stack.length-1].toString().length<=1) {
+            stack.pop()
+        } else {
+            stack[stack.length-1] = stack[stack.length-1].substring(0, stack[stack.length-1].length - 1)
+            }
+        }
+        stack_pointer = stack.length>0? stack.length - 1 : stack_pointer
+        
+    }
 
-
-}
-
-
-// function clear() {
-//     stack.splice(0, stack.length)
-//     this.running = 0
-// }
 
 
 function history() {
@@ -132,9 +120,8 @@ function history() {
 
 
 function ac() {
-    stack.length = 0
+    stack = [""]
     stack_pointer = 0
-    running = 0
+    result_screen.value = 0
 
 }
-
