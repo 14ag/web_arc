3ce8257a0d64da77788f69b681c76c697f96e248
14ag <muriukipn@gmail.com>
2025-08-10 23:47:41 +0300

    refactor: improve code consistency and readability in Calculator class

--- calculator/app.js
+++ calculator/app.js
@@ -1,14 +1,14 @@
 //uses classes
 class Calculator {
     //get elements by id class tag ... do query selector (all)
-    static buttons = Array.from(document.querySelectorAll('button'))
+    static buttons = Array.from(document.querySelectorAll('button'));
     static input_screen = document.querySelector('.input');
     static result_screen = document.querySelector('.result');
-    static history_content = document.querySelector('.history_content')
-    static card = document.querySelector('.history')
-    static stack0
-    static stack = [""]
-    static stack_pointer = 0 //stack pointer
+    static history_content = document.querySelector('.history_content');
+    static card = document.querySelector('.history');
+    static stack0;
+    static stack = [""];
+    static stack_pointer = 0; //stack pointer
     static sign = { //property/key :value
         "*": (a, b) => {
             return a * b;
@@ -21,7 +21,7 @@ class Calculator {
         },
         "/": (a, b) => {
             if (b == 0) {
-                throw new Error("dividing by zero????");
+                throw new Error("dividing by zero????");  // refactor this so that operation stops on error
             }
             return a / b
         },
@@ -29,7 +29,7 @@ class Calculator {
 
 
     // order of opps stated here
-    static order = Object.keys(sign).sort(function (a, b) {
+    static order = Object.keys(Calculator.sign).sort(function (a, b) {
         let O = ["/", "*", "+", "-"] //here 
         if (O.indexOf(a) < O.indexOf(b)) {
             return -1
@@ -38,42 +38,26 @@ class Calculator {
         }
     })
 
-//create elements, attributes, tags
-buttons.forEach(function(x) {
-        x.addEventListener('click',
-            function () {
-                // input_screen.value = x.value
-                input_handler(x.value, x.name, x.className)
-                if (x.name != "equals") {
-                    input_screen.value = stack.join("")
-                }
-
-            })
-    })
-
-
-
-
 
 
 
     static input_handler(x = "", y = "", z = "") { //x- value,  y-name,  z-class
-        x = x.toString()
-        if (y == "n") {
-            if (order.includes(stack[stack_pointer])) {
-                stack_pointer++
+        x = x.toString();
+        if (y == "n") { // handles numbers
+            if (Calculator.order.includes(Calculator.stack[Calculator.stack_pointer])) {
+                Calculator.stack_pointer++
             }
-            if (stack[stack_pointer] == undefined) {
-                stack[stack_pointer] = ""
+            if (Calculator.stack[Calculator.stack_pointer] == undefined) {
+                Calculator.stack[Calculator.stack_pointer] = ""
             }
-            stack[stack_pointer] += x
+            Calculator.stack[Calculator.stack_pointer] += x
         } else {
             switch (z) { //classname
                 case "operator":
                     //add the sign
-                    if (stack[stack_pointer] != 0) {
-                        stack.push(x)
-                        stack_pointer++
+                    if (Calculator.stack[Calculator.stack_pointer] != 0) {
+                        Calculator.stack.push(x)
+                        Calculator.stack_pointer++
                     }
                     break;
                 case "operation":
@@ -85,29 +69,29 @@ buttons.forEach(function(x) {
 
     static equals() {
 
-        stack0 = stack.join(""); //to be used in history
+        Calculator.stack0 = Calculator.stack.join(""); //to be used in history
 
         //check if last char is NaN
-        if (order.includes(stack[stack_pointer])) {
+        if (Calculator.order.includes(Calculator.stack[Calculator.stack_pointer])) {
             return;
         }
 
-        for (let i = 0; i < order.length; i++) {
+        for (let i = 0; i < Calculator.order.length; i++) {
             //get no. occurences
-            let counter = stack.filter((x) => x == order[i]).length
+            let counter = Calculator.stack.filter((x) => x == Calculator.order[i]).length
 
             //loop over array looking for order[i] 
             for (let ii = 0; ii < counter; ii++) {
-                let sub_op = stack.indexOf(order[i]) //a number eg 1
+                let sub_op = Calculator.stack.indexOf(Calculator.order[i]) //a number eg 1
                 // then take found[+1] and [-1] then pass them to be evaluated in the sign object
                 //stack[sub_op] is currentOperatorSymbol= 
-                stack.splice(sub_op - 1, 3, sign[stack[sub_op]](+stack[sub_op - 1], +stack[sub_op + 1]))
+                Calculator.stack.splice(sub_op - 1, 3, Calculator.sign[Calculator.stack[sub_op]](+Calculator.stack[sub_op - 1], +Calculator.stack[sub_op + 1]))
             }
         }
 
-        stack_pointer = 0
-        result_screen.value = stack.join("")
-        history_content.innerHTML += `<p><span class="q">${stack0}</span><br><span class="a"><b>${stack}</b></span></p>`
+        Calculator.stack_pointer = 0
+        Calculator.result_screen.value = Calculator.stack.join("")
+        Calculator.history_content.innerHTML += `<p><span class="q">${Calculator.stack0}</span><br><span class="a"><b>${Calculator.stack}</b></span></p>`
     }
 
 
@@ -121,10 +105,25 @@ buttons.forEach(function(x) {
     }
 
     static ac() {
-        stack = [""]
-        stack_pointer = 0
-        result_screen.value = 0
-
+        Calculator.stack = [""];
+        Calculator.stack_pointer = 0;
+        Calculator.result_screen.value = 0;
+        Calculator.input_screen.value = "";
     }
 
+    
+    static init() {
+        // Add event listeners to all buttons
+        Calculator.buttons.forEach(function (button) {
+            button.addEventListener('click', function () {
+                Calculator.input_handler(button.value, button.name, button.className);
+                if (button.name !== "equals") {
+                    Calculator.input_screen.value = Calculator.stack.join("");
+                }
+            });
+        });
+    }
 }
+
+// Initialize the calculator event listeners
+Calculator.init();
